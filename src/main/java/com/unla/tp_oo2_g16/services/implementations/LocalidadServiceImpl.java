c69d184d379560ec2a6c05aba67751411fd351fc
mar3z <marez.gitex@gmail.com>
2025-06-19 17:08:38 -0300

    Correción final Recuperatorio

--- src/main/java/com/unla/tp_oo2_g16/services/implementations/LocalidadServiceImpl.java
+++ src/main/java/com/unla/tp_oo2_g16/services/implementations/LocalidadServiceImpl.java
@@ -1,5 +1,8 @@
 package com.unla.tp_oo2_g16.services.implementations;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.unla.tp_oo2_g16.models.entities.Localidad;
@@ -12,22 +15,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LocalidadServiceImpl implements LocalidadServiceInterface{
 
-    private final LocalidadRepository localidadRepository;
+    @Autowired
+    private LocalidadRepository localidadRepository;
 
     @Override
-    public Localidad findById(int id){
-        return localidadRepository.findById(id);
+    public List<Localidad> findAll() {
+        return localidadRepository.findAll();
     }
 
     @Override
-    public Localidad findByLocalidad(String localidad){
-        return localidadRepository.findByLocalidad(localidad);
+    public Localidad findById(Integer id) {
+        return localidadRepository.findById(id).orElse(null);
     }
 
     @Override
-    public Localidad save(Localidad localidad){
+    public Localidad save(Localidad localidad) {
         return localidadRepository.save(localidad);
     }
 
+    @Override
+    public void deleteById(Integer id) {
+        localidadRepository.deleteById(id);
+    }
 
 }
--- src/main/java/com/unla/tp_oo2_g16/services/interfaces/LocalidadServiceInterface.java
+++ src/main/java/com/unla/tp_oo2_g16/services/interfaces/LocalidadServiceInterface.java
@@ -1,12 +1,14 @@
 package com.unla.tp_oo2_g16.services.interfaces;
 
+import java.util.List;
+
 import com.unla.tp_oo2_g16.models.entities.Localidad;
 
 public interface LocalidadServiceInterface {
 
-    Localidad findById(int id);
-    Localidad findByLocalidad(String localidad);
-
+    Localidad findById(Integer id);
+    void deleteById(Integer id);
+    List<Localidad> findAll();
     Localidad save(Localidad localidad);
 
 }
